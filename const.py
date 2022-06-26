897f7b58f6f880842aa451836f4fd9efb0920abc
kellermanmota <kellerman@discente.ufg.com.br>
2022-06-26 17:05:39 +0000

    novos arquivos

---
+++ const.py
@@ -0,0 +1,5 @@
+CHAT_SERVER_HOST = "chat-server"
+CHAT_SERVER_PORT = 5000
+
+# IDs de usuários registrados com os respectivos endereços IP e números de porta
+registry = {"Alice":("chat-client-01",5001), "Bob":("chat-client-02",5002)}
